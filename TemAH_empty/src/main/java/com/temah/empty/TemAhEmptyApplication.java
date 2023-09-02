772e36bb2d5f9ec9bfc9cfe8ea3dd1d55b44d4f7
wanglei <lehn126@126.com>
2023-09-02 10:35:13 +0800

    Rename package name in template project

--- TemAH_empty/src/main/java/com/temah/empty/TemAhEmptyApplication.java
+++ TemAH_empty/src/main/java/com/temah/empty/TemAhEmptyApplication.java
@@ -1,4 +1,4 @@
-package com.temah.temp;
+package com.temah.empty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- TemAH_empty/src/test/java/com/temah/empty/TemAhEmptyApplicationTests.java
+++ TemAH_empty/src/test/java/com/temah/empty/TemAhEmptyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.temah.temp;
+package com.temah.empty;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
